513ea48a1192aa8fc59d1ad4c3bcf77aa0a78ecd
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-24 17:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let age=document.getElementById("age");
+let name=document.getElementById("name");
+// if(age.value==null || name.value==null){
+// 	alert("Please Enter valid details")
+// }
+
+let promise= new promise((resolve,reject)=>{
+	if(age.innerText>=18){
+		setTimeout(()=>{
+			resolve(alert(`Welcome,${name.innerText} . You can vote.`))
+		,4000)
+	}else{
+		reject(alert(`Oh sorry${name.innerText}. You aren't old enough.`))
+	}
+})
+
+
+
+
+	
