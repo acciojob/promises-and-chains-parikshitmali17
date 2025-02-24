ad946bca9084bc526cc710b960cb9ceafc5ebd0b
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-24 17:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 let age=document.getElementById("age");
 let name=document.getElementById("name");
-// if(age.value==null || name.value==null){
-// 	alert("Please Enter valid details")
-// }
+if(age.value==null || name.value==null){
+	alert("Please Enter valid details")
+}
 
-let promise= new promise((resolve,reject)=>{
+const function promise= new promise((resolve,reject)=>{
 	if(age.innerText>=18){
 		setTimeout(()=>{
 			resolve(alert(`Welcome,${name.innerText} . You can vote.`))
