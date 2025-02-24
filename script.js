aed2cb8ddea0be1f5e976849278fa2ef7c1d8930
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-24 17:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ let name=document.getElementById("name");
 if(age.value==null || name.value==null){
 	alert("Please Enter valid details")
 }
-	return
-})
-
-const function promise= new promise((resolve,reject)=>{
+ const promise= new promise((resolve,reject)=>{
 setTimeout(()=>{
 		if(age.value>=18){
 		
-			resolve(alert(`Welcome,${name.value} . You can vote.`))
+			resolve(`Welcome,${name.value}. You can vote.`)
 		
 	}else{
-		reject(alert(`Oh sorry${name.value}. You aren't old enough.`))
+		reject(`Oh sorry${name.value}. You aren't old enough.`)
 	}
 },4000)
 })
 
-
-
+promise.this((message)=>{
+	alert(message)
+}).catch((message)=>{
+	alert(message)
+})
+	
 
 	
